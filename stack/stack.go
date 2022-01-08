23e1903a4475a5ac14d98c1e41a0ffba47436bd6
Osama Mosaad <osama.mosaad@basharsoft.com>
2022-01-08 17:14:50 +0200

    Added Doc for Stack package

--- stack/stack.go
+++ stack/stack.go
@@ -9,15 +9,19 @@ type stack struct {
 	elements []interface{}
 }
 
+// New to initalize stack and set the top with default value -1 which means empty.
 func New() stack {
 	return stack{top: -1}
 }
 
+// Push to add an item into the stack
 func (s *stack) Push(item interface{}) {
 	s.top++
 	s.elements = append(s.elements, item)
 }
 
+// Pop to remove an item from the stack
+// returns error if the stack is empty
 func (s *stack) Pop() error {
 	if !s.IsEmpty() {
 		s.elements = s.elements[:s.top]
@@ -27,6 +31,8 @@ func (s *stack) Pop() error {
 	return errors.New("stack is empty")
 }
 
+// Peek get the stack peek
+// returns nil and error when the stack is empty
 func (s stack) Peek() (interface{}, error) {
 	if !s.IsEmpty() {
 		return s.elements[s.top], nil
@@ -34,10 +40,13 @@ func (s stack) Peek() (interface{}, error) {
 	return nil, errors.New("stack is empty")
 }
 
+// IsEmpty to check whether the stack is empty
 func (s stack) IsEmpty() bool {
 	return s.top < 0
 }
 
+// List to retrun the stack data as an array
+// returns nil, with error if the stack is empty
 func (s stack) List() ([]interface{}, error) {
 	if s.IsEmpty() {
 		return nil, errors.New("stack is empty")
